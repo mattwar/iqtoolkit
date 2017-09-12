9378b3065cf7b19dcac015389ee3c54e5976f5d0
Matt Warren <mattwar@microsoft.com>
2017-09-12 13:38:16 -0700

    update nuspecs

--- nuget/pack.cmd
+++ nuget/pack.cmd
@@ -1,7 +1,7 @@
-nuget pack IQToolkit.Data.nuspec -OutputDirectory "..\bin\release\nupkgs"
-nuget pack IQToolkit.Data.Access.nuspec -OutputDirectory "..\bin\release\nupkgs"
-nuget pack IQToolkit.Data.MySqlClient.nuspec -OutputDirectory "..\bin\release\nupkgs"
-nuget pack IQToolkit.Data.SqlClient.nuspec -OutputDirectory "..\bin\release\nupkgs"
-nuget pack IQToolkit.Data.SQLite.nuspec -OutputDirectory "..\bin\release\nupkgs"
-nuget pack IQToolkit.Data.SqlServerCe.nuspec -OutputDirectory "..\bin\release\nupkgs"
+nuget pack IQToolkit.Common.nuspec -OutputDirectory "..\bin\release\nupkgs"
+nuget pack IQToolkit.Access.nuspec -OutputDirectory "..\bin\release\nupkgs"
+nuget pack IQToolkit.MySqlClient.nuspec -OutputDirectory "..\bin\release\nupkgs"
+nuget pack IQToolkit.SqlClient.nuspec -OutputDirectory "..\bin\release\nupkgs"
+nuget pack IQToolkit.SQLite.nuspec -OutputDirectory "..\bin\release\nupkgs"
+nuget pack IQToolkit.SqlServerCe.nuspec -OutputDirectory "..\bin\release\nupkgs"
 
